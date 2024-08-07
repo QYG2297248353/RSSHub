02282a97f592a33490ded675b34e54b12058df9e
萌森 <qyg2297248353@163.com>
2024-08-07 19:05:46 +0800

    feat: base fsm

---
+++ lib/routes/pt-fsm/namespace.ts
@@ -0,0 +1,11 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'FSM',
+    url: 'fsm.me',
+    description: `
+:::tip
+该应用为 [FSM](https://fsm.me) 的 RSS 订阅源。
+需要授权密钥才能订阅，密钥可以在 [FSM](https://fsm.me) 的设置页面找到。
+:::`,
+};
